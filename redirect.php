0626902e90f4d49f74352e4e3c2aa1b7fa498d9e
khusnia <61143169+khusnia@users.noreply.github.com>
2020-06-09 18:25:07 +0700

    Add files via upload

---
+++ redirect.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Redirecting</title>
+  <meta http-equiv="refresh" content="2; url= https://aegis18.000webhostapp.com/beranda/beranda.php">
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<main role="main" class="container">
+
+  <div class="starter-template">
+    <h1>Selamat Datang di Web SIBAKMIN Program Studi Sistem Informasi</h1>
+    <p class="lead">Sedang Menuju Ke halaman, Silahkan tunggu... </p>
+  </div>
+
+</main>
+</body>
+</html>
