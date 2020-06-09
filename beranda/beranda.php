13cba44a459295cbbbae5aa2a4f593de604a7ceb
khusnia <61143169+khusnia@users.noreply.github.com>
2020-06-09 18:21:11 +0700

    Add files via upload

---
+++ beranda/beranda.php
@@ -0,0 +1,394 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>Sistem Informasi</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+<meta name="generator" content="Jekyll v3.8.5">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/starter-template/">
+<style>
+body {font-family: "Lato", sans-serif}
+.mySlides {display: none}
+</style>
+<body>
+<div class="w3-top">
+  <div class="w3-bar w3-light-blue w3-card">
+    <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#beranda" class="w3-bar-item w3-button w3-padding-large"><font color="white">Beranda</font></a>
+    <a href="#member" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><font color="white">Prestasi</font></a>
+    <a href="#gallery" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><font color="white">Foto</font></a>
+    <a href="#kritik" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><font color="white">Kritik dan Saran</font></a>
+    <a href="../login/login.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small w3-right"><font color="white">Input/Cetak data</font></a>
+  </div>
+</div>
+<div class="w3-content" style="max-width:2000px;margin-top:46px">
+
+  <div class="mySlides w3-display-container w3-center">
+    <img src="foto/tama.jpg" style="width:500px">
+    <div class="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
+    </div>
+  </div>
+  <div class="mySlides w3-display-container w3-center">
+    <img src="foto/cready.jpg" style="width:500px">
+    <div class="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
+    </div>
+  </div>
+  <div class="mySlides w3-display-container w3-center">
+    <img src="foto/ibra.png" style="width:500px">
+    <div class="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
+    </div>
+  </div>
+
+
+  <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="beranda">
+    <h2 class="w3-wide">Sistem Informasi</h2>
+    <p class="w3-opacity"><i>Universitas Pembangunan Nasional "Veteran" Jawa Timur</i></p>
+    <p class="w3-justify">Sistem Informasi merupakan salah satu bidang studi dalam rumpun ilmu Informatika. Sistem Informasi pada pengaplikasiannya merupakan integrasi antara teknologi informasi dan bisnis, sehingga keberadaan sebuah Sistem Informasi diharapkan memberikan kontribusi positif maupun keuntungan bagi instansi, organisasi, maupun perusahaan. Walaupun program studi ini terbilang masih cukup baru, namun program studi Sistem Informasi khususnya di UPN "Veteran" Jawa Timur ini memiliki banyak sekali mahasiswa dengan segudang prestasi yang dapat mengharumkan nama baik Sistem Informasi khususnya nama baik Universitas Pembangunan Nasional "Veteran" Jawa Timur hingga ke tingkat nasional.</p>
+    <div class="w3-row w3-padding-32">
+      <div class="w3-third">
+        <p>Koorprogdi</p>
+        <img src="foto/pak_cahyo.jpg" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+        <p>NUR CAHYO WIBOWO, S.Kom, M.Kom</p>
+      </div>
+      <div class="w3-third">
+        <p>Dosen</p>
+        <img src="foto/pak_agus.jpg" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+        <p>Dr. Eng. AGUSSALIM, M.T.</p>
+      </div>
+      <div class="w3-third">
+        <p>Ketua Himpunan</p>
+        <img src="foto/tata.jpg" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+        <p>Talytakum Suryah Naibaho</p>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="w3-light-blue" id="member">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+      <h2 class="w3-wide w3-center">Gudang Prestasi Mahasiswa</h2>
+      <p class="w3-opacity w3-center"><i>Program Studi Sistem Informasi</i></p><br>
+      <p class="w3-opacity w3-left"><i>*Apabila ada kesalahan harap hubungi admin</i></p><br>
+      <div class="w3-row-padding w3-padding-32" style="margin:0 -16px">
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/cready.jpg" alt="New York" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Cready Celgie Gildbrandsen</b></p>
+            <p class="w3-opacity">Angkatan : 2018</p>
+            <p>Prestasi : Juara 1 Kyorugi Senior U-63kg</p>
+            <p>Kejurnas Taekwondo Profesional Bela Negara (Jabar Open) 2020</p>
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/ibra.png" alt="Paris" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Ibrahim</b></p>
+            <p class="w3-opacity">Angkatan : 2018</p>
+            <p>Prestasi : Semifinalis</p>
+            <p>L.A Campus League Basketball 2018</p>
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/tiara.png" alt="San Francisco" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Tiara Karunia Miranti</b></p>
+            <p class="w3-opacity">Angkatan : 2018</p>
+            <p>Prestasi : Harapan 1 Sanitasi dan Kesehatan</p>
+            <p>BALARAM XXVII KSR UB Tingkat Nasional</p>
+          </div>
+        </div>
+      </div>
+      <div class="w3-row-padding w3-padding-32" style="margin:0 -16px">
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/jimbon.jpg" alt="New York" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Ananda Lakunti Ardiatama</b></p>
+            <p class="w3-opacity">Angkatan : 2018</p>
+            <p>Prestasi : Juara 1 Vlog Polresta Sidoarjo 2019 dan Juara 2 Video Pendek Sidoresik</p>
+            <p>Lomba Vlog dan Video Pendek Polresta Sidoarjo 2019</p>
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/anto.jpg" alt="Paris" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Sudewantoro Nur Muhammad</b></p>
+            <p class="w3-opacity">Angkatan : 2018</p>
+            <p>Prestasi : Juara 3</p>
+            <p>Pekan Olahraga Provinsi (PORPROV) VI JATIM </p>
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/aurel.jpg" alt="San Francisco" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Auralia Miffatul Jannah</b></p>
+            <p class="w3-opacity">Angkatan : 2018</p>
+            <p>Prestasi : Best Make-Up</p>
+            <p>Make-Up Contest</p>
+          </div>
+        </div>
+      </div>
+      <div class="w3-row-padding w3-padding-32" style="margin:0 -16px">
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/baitun.JPG" alt="New York" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Baitun Nadhiroh</b></p>
+            <p class="w3-opacity">Angkatan : 2018</p>
+            <p>Prestasi : Juara 3 Futsal Putri</p>
+            <p>Olimpiade Veteran UPN JATIM</p>
+            </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/diah.jpg" alt="New York" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Diah Fatmawati</b></p>
+            <p class="w3-opacity">Angkatan : 2018</p>
+            <p>Prestasi : 15 Besar Se-Provinsi JATIM bidang Enviromental Science</p>
+            <p>Penelitian Belia (Center For Youth Scientists) 2016</p>
+          </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/rahma.jpg" alt="New York" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Anisa Rahma Salsabila</b></p>
+            <p class="w3-opacity">Angkatan : 2019</p>
+            <p>Prestasi : Sebagai Peserta</p>
+            <p>Lomba Menulis Opini Kemdikbud Pusat Prestasi Nasional</p>
+            </div>
+        </div>
+      </div>
+      <div class="w3-row-padding w3-padding-32" style="margin:0 -16px">
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/ainun.jpg" alt="New York" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Ainun Rizkiyani Fadillah</b></p>
+            <p class="w3-opacity">Angkatan : 2019</p>
+            <p>Prestasi : Sebagai Peserta</p>
+            <p>Lomba Poster</p>
+            </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/nia.jpeg" alt="New York" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Oktania Purwaningrum</b></p>
+            <p class="w3-opacity">Angkatan : 2018</p>
+            <p>Prestasi : Penulis Terpilih</p>
+            <p>Lomba Menulis Puisi dengan tema "Rumah"</p>
+            </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/tama.jpg" alt="New York" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Tamariska Laras Suci</b></p>
+            <p class="w3-opacity">Angkatan : 2019</p>
+            <p>Prestasi : Juara 3</p>
+            <p>Piala Walikota Malang Cabor Hockey</p>
+            </div>
+        </div>
+        <div class="w3-third w3-margin-bottom">
+          <img src="foto/abu.jpeg" alt="New York" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>Moch. Aburizal Fatwa</b></p>
+            <p class="w3-opacity">Angkatan : 2018</p>
+            <p>Prestasi : Juara 1 Basket Putra</p>
+            <p>Fasilkom Fest 2019</p>
+            </div>
+      </div>
+      </div>
+    </div>
+    <div class="w3-light-blue" id="diagram">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+      <h2 class="w3-wide w3-center">Diagram Jumlah Prestasi</h2>
+      <p class="w3-opacity w3-center"><i>"Diagram Pie"</i></p><br>
+      <?php
+include('../koneksi.php');
+$produk = mysqli_query($koneksi,"select * from tb_total");
+while($row = mysqli_fetch_array($produk)){
+	$nama_produk[] = $row['angkatan'];
+	
+	$query = mysqli_query($koneksi,"select sum(jumlah) as jumlah from tb_total where id='".$row['id']."'");
+	$row = $query->fetch_array();
+	$jumlah_produk[] = $row['jumlah'];
+}
+?>
+<!doctype html>
+<html>
+
+<head>
+	<title>Pie Chart</title>
+	<script type="text/javascript" src="Chart.js"></script>
+</head>
+
+<body>
+	<div id="canvas-holder" style="width:100%">
+		<canvas id="chart-area"></canvas>
+	</div>
+	<script>
+		var config = {
+			type: 'doughnut',
+			data: {
+				datasets: [{
+					data:<?php echo json_encode($jumlah_produk); ?>,
+					backgroundColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'black',
+					'pink',
+					'green',
+					'white',
+					'orange',
+					'silver'
+					],
+					borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'black',
+					'pink',
+					'green',
+					'white',
+					'orange',
+					'silver'
+					],
+					label: 'Presentase Penjualan Barang'
+				}],
+				labels: <?php echo json_encode($nama_produk); ?>},
+			options: {
+				responsive: true
+			}
+		};
+
+		window.onload = function() {
+			var ctx = document.getElementById('chart-area').getContext('2d');
+			window.myPie = new Chart(ctx, config);
+		};
+
+		document.getElementById('randomizeData').addEventListener('click', function() {
+			config.data.datasets.forEach(function(dataset) {
+				dataset.data = dataset.data.map(function() {
+					return randomScalingFactor();
+				});
+			});
+
+			window.myPie.update();
+		});
+
+		var colorNames = Object.keys(window.chartColors);
+		document.getElementById('addDataset').addEventListener('click', function() {
+			var newDataset = {
+				backgroundColor: [],
+				data: [],
+				label: 'New dataset ' + config.data.datasets.length,
+			};
+
+			for (var index = 0; index < config.data.labels.length; ++index) {
+				newDataset.data.push(randomScalingFactor());
+
+				var colorName = colorNames[index % colorNames.length];
+				var newColor = window.chartColors[colorName];
+				newDataset.backgroundColor.push(newColor);
+			}
+
+			config.data.datasets.push(newDataset);
+			window.myPie.update();
+		});
+
+		document.getElementById('removeDataset').addEventListener('click', function() {
+			config.data.datasets.splice(0, 1);
+			window.myPie.update();
+		});
+	</script>
+</body>
+
+</html>
+    </div>
+  </div>
+  </div>
+  <div class="w3-container w3-content w3-padding-64" style="max-width:800px" id="gallery">
+    <h2 class="w3-wide w3-center">Foto Mahasiswa</h2>
+    <p class="w3-opacity w3-center"><i>An unforgettable memory</i></p>
+    <div class="w3-row w3-padding-32">
+      <div class="w3-third">
+        <img src="foto/bareng1.JPG" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+      </div>
+      <div class="w3-third">
+        <img src="foto/bareng2.JPG" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+      </div>
+      <div class="w3-third">
+        <img src="foto/bareng3.JPG" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+      </div>
+    </div>
+    <div class="w3-row w3-padding-32">
+      <div class="w3-third">
+        <img src="foto/bareng4.png" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+      </div>
+      <div class="w3-third">
+        <img src="foto/bareng5.JPG" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+      </div>
+      <div class="w3-third">
+        <img src="foto/bareng6.JPG" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+      </div>
+    </div>
+    <div class="w3-row w3-padding-32">
+      <div class="w3-third">
+        <img src="foto/bareng7.JPG" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+      </div>
+      <div class="w3-third">
+        <img src="foto/bareng8.JPG" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+      </div>
+      <div class="w3-third">
+        <img src="foto/bareng9.jpg" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+      </div>
+    </div>
+    <div class="w3-row w3-padding-32">
+      <div class="w3-third">
+        <img src="foto/bareng10.jpg" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+      </div>
+      <div class="w3-third">
+        <img src="foto/bareng11.png" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+      </div>
+      <div class="w3-third">
+        <img src="foto/bareng12.jpg" class="w3-round w3-margin-bottom" alt="Random Name" style="width:80%">
+      </div>
+    </div>
+  </div>
+  <div class="w3-light-blue" id="kritik">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+      <h2 class="w3-wide w3-center">Kritik dan Saran</h2>
+      <p class="w3-opacity w3-center"><i>"your one opinion is very valuable"</i></p><br>
+      <div class="w3-row w3-padding-32">
+      <div class="w3-col m6 w3-large w3-margin-bottom">
+        <i class="fa fa-vcard-o" style="width:30px"></i>Nama :<br>
+        <i class="fa fa-comment-o" style="width:30px"></i>Kritik dan saran :<br>
+        <i class="fa fa-envelope-o" style="width:30px"> </i> Email :<br>
+      </div>
+      <div class="w3-col m6">
+        <form method="Post" action="../kritik/insertkritik.php" target="_blank">
+          <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
+            <div class="w3-half">
+              <input class="w3-input w3-border" type="text" placeholder="Nama" required name="nama">
+            </div>
+            <div class="w3-half">
+              <input class="w3-input w3-border" type="text" placeholder="Email" required name="email">
+            </div>
+          </div>
+          <input class="w3-input w3-border" type="text" placeholder="Kritik dan Saran" required name="kritik">
+          
+          
+        </form>
+      </div>
+    </div>
+      <div class="w3-row-padding w3-padding-32" style="margin:0 -16px"></div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
