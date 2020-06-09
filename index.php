f13c5e8504921a3410cd4d93fd908f827f1850f9
khusnia <61143169+khusnia@users.noreply.github.com>
2020-06-09 18:22:45 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html <html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
+<head>
+	<title>Selamat Datang</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+
+    <div class="fadeIn first">
+      <img src="logo_si.png" id="icon" alt="User Icon" height="250">
+    </div>
+
+    <form method="get" action="redirect.php">
+      <h2>Selamat datang di Web</h2>
+      <h3>SIBAKMIN</h3>
+      <h4>(Sistem Informasi Bakat Minat Prestasi)</h4>
+      <br>
+      <font style="font-size:20px"><i>Program Studi Sistem Infomasi</i>
+      <br>
+      <font style="font-size:20px"><i>Universitas Pembangunan Nasional "Veteran" Jawa Timur</i>
+      <br>
+      <button type="submit" class="fadeIn fourth" >Masuk</button>
+    </form>
+
+  </div>
+</div>
+</body>
+</html>
